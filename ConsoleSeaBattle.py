a5a836372caf3310e52367d1719d43063dc6ed3b
Swotty <swotty@yandex.ru>
2023-04-10 11:01:57 +0300

    Mod and New File

--- ConsoleSeaBattle.py
+++ ConsoleSeaBattle.py
@@ -1,13 +1,196 @@
 """
-Не закончена. Все что есть - это генерация одного поля.
-Остановился на создании второго поля. Половина классов просто есть, так как не знаю как их использовать.
-
-После разбора и просмотра кода игры с вебинара данная задача не решаема,
-если ты не практиковал классы минимум год после их изучения. Мне больше нечего сказать к этой задаче для "начинающих"
+----В процессе разработки----
 """
 
+from abc import ABC
 import random
 import os
+
+
+class OffField(Exception):
+    pass
+
+
+class InSameDot(Exception):
+    pass
+
+
+class Ships(ABC):
+    def __init__(self, nose_dot: tuple, width: int, direction: str, hp: int, type_ship: str):
+        self.nose_dot: tuple = nose_dot
+        self.width: int = width
+        self.direction: str = direction
+        self.hp: int = hp
+        self.type_ship: str = type_ship
+        self.dots = []
+
+    def __repr__(self):
+        return f"{self.width} {self.nose_dot} {self.direction} {self.hp} {self.type_ship}"
+
+    def get_ship_nose(self):
+        return self.nose_dot
+
+    def get_ship_direction(self):
+        return f"{self.direction}"
+
+
+class Battleship(Ships):
+    def __init__(self, nose_dot: tuple, width=3, direction='HORIZONTAL', hp=3, type_ship="Линкор"):
+        super().__init__(nose_dot, width, direction, hp, type_ship)
+
+
+class Cruiser(Ships):
+    def __init__(self, nose_dot: tuple, width=2, direction='HORIZONTAL', hp=2, type_ship="Крейсер"):
+        super().__init__(nose_dot, width, direction, hp, type_ship)
+
+
+class Speedboat(Ships):
+    def __init__(self, nose_dot: tuple, width=1, direction='HORIZONTAL', hp=1, type_ship="Катер"):
+        super().__init__(nose_dot, width, direction, hp, type_ship)
+
+
+class Board:
+    def __init__(self, hid: bool):
+        self.hid = hid
+        self.all_hp = 12
+        self.letters = [" ", "A", "B", "C", "D", "E", "F"]
+        self.numbers = ["1", "2", "3", "4", "5", "6"]
+        self.battle_field = [[" "] * 6 for _ in range(6)]
+
+        self.check_near = "T"
+        self.ship_object = "■"
+
+        self.ships_dict = []
+        self.ships_on_desk = []
+        self.dots_ships = []
+        self.for_near_dict = []
+
+    def add_ship(self):
+        self.for_near_dict.clear()
+        while True:
+            choice_ship = 1
+            battleship = Battleship((random.randrange(0, 6), random.randrange(0, 6)),
+                                    direction=(random.choice(("VERTICAL", "HORIZONTAL"))))
+
+
+            cruiser = Cruiser((random.randrange(0, 6), random.randrange(0, 6)),
+                              direction=(random.choice(("VERTICAL", "HORIZONTAL"))))
+            speedboat = Speedboat((random.randrange(0, 6), random.randrange(0, 6)),
+                                  direction=(random.choice(("VERTICAL", "HORIZONTAL"))))
+
+
+            if choice_ship == 1:
+                now_place = battleship
+            elif choice_ship < 4:
+                now_place = cruiser
+            else:
+                now_place = speedboat
+
+
+            if now_place.direction == "HORIZONTAL":
+                if now_place.nose_dot[0] + 2 > 5:
+                    continue
+                for j in range(3):
+                    self.battle_field[now_place.nose_dot[0] + j][now_place.nose_dot[1]] = self.ship_object
+                    self.dots_ships.append((now_place.nose_dot[0] + j, now_place.nose_dot[1]))
+
+                    self.for_near_dict.append((now_place.nose_dot[0] + j, now_place.nose_dot[1]))
+            else:
+                if now_place.nose_dot[1] + 2 > 5:
+                    continue
+                for j in range(3):
+                    self.battle_field[now_place.nose_dot[0]][now_place.nose_dot[1] + j] = self.ship_object
+                    self.dots_ships.append((now_place.nose_dot[0], now_place.nose_dot[1] + j))
+
+                    self.for_near_dict.append((now_place.nose_dot[0], now_place.nose_dot[1] + j))
+
+            choice_ship += 1
+            self.ships_dict.append([now_place.width, now_place.nose_dot, now_place.direction,
+                                    now_place.hp, now_place.type_ship])
+            self.near_ship(now_place.direction)
+            break
+
+    def retry_place(self):
+        self.ships_dict.clear()
+        i1, j1 = 0, 0
+        for i in self.battle_field:
+            for j in i:
+                if j == self.ship_object or j == self.check_near:
+                    self.battle_field[i1][j1] = " "
+                j1 += 1
+            i1 += 1
+            j1 = 0
+
+    def near_ship(self, ship_direction):
+        for i, j in self.for_near_dict:
+            if ship_direction == "HORIZONTAL":
+                if i != 0 and self.battle_field[i - 1][j] != self.ship_object:
+                    self.battle_field[i - 1][j] = self.check_near
+                    if j - 1 >= 0:
+                        self.battle_field[i - 1][j - 1] = self.check_near
+                    if j + 1 < 6:
+                        self.battle_field[i - 1][j + 1] = self.check_near
+                if i != 5 and self.battle_field[i + 1][j] != self.ship_object:
+                    self.battle_field[i + 1][j] = self.check_near
+                    if j + 1 < 6:
+                        self.battle_field[i + 1][j + 1] = self.check_near
+                    if j - 1 >= 0:
+                        self.battle_field[i + 1][j - 1] = self.check_near
+                if j != 5:
+                    self.battle_field[i][j + 1] = self.check_near
+                if j != 0:
+                    self.battle_field[i][j - 1] = self.check_near
+
+            elif ship_direction == "VERTICAL":
+                if j != 0 and self.battle_field[i][j - 1] != self.ship_object:
+                    self.battle_field[i][j - 1] = self.check_near
+                    if i - 1 >= 0:
+                        self.battle_field[i - 1][j - 1] = self.check_near
+                    if i + 1 < 6:
+                        self.battle_field[i + 1][j - 1] = self.check_near
+                if j != 5 and self.battle_field[i][j + 1] != self.ship_object:
+                    self.battle_field[i][j + 1] = self.check_near
+                    if i + 1 < 6:
+                        self.battle_field[i + 1][j + 1] = self.check_near
+                    if i - 1 >= 0:
+                        self.battle_field[i - 1][j + 1] = self.check_near
+                if i != 5:
+                    self.battle_field[i + 1][j] = self.check_near
+                if i != 0:
+                    self.battle_field[i - 1][j] = self.check_near
+        self.for_near_dict.clear()
+
+    def shot(self, a1, a2):
+        if self.battle_field[a1][a2] == self.ship_object:
+            self.battle_field[a1][a2] = "x"
+            self.all_hp -= 1
+        elif self.battle_field[a1][a2] == "•":
+            raise InSameDot
+        else:
+            self.battle_field[a1][a2] = "•"
+
+    def print_field(self):
+        for i in self.letters:
+            print(i, "  ", end="")
+        print()
+        _ = 1
+        for i in self.battle_field:
+            print(_, end=" | ")
+            for j in i:
+                print(j, "| ", end="")
+            _ += 1
+            print()
+        return ""
+
+
+class PlayerBoard(Board):
+    def __init__(self, hid):
+        super().__init__(hid)
+
+
+class AIBoard(Board):
+    def __init__(self, hid):
+        super().__init__(hid)
 
 
 class Dot:
@@ -19,218 +202,37 @@ class Dot:
         return self.x == other.x and self.y == other.y
 
 
-class Ship:
-    def __init__(self, width: int, nose_dot: tuple, direction: str, hp: int):
-        self.width: int = width
-        self.nose_dot: tuple = nose_dot
-        self.direction: str = direction
-        self.hp: int = hp
+player_field = Board(False)
+bot_field = Board(True)
 
-    def dots(self):
-        return  # dots
+player_field.add_ship()
+bot_field.add_ship()
 
-    def get_ship(self):
-        return f"{self.width} {self.nose_dot} {self.direction} {self.hp}"
+print(player_field.ships_dict)
+print(bot_field.ships_dict)
 
-    def get_ship_nose(self):
-        return self.nose_dot
+print(player_field.dots_ships)
+print(bot_field.dots_ships)
 
-    def get_ship_direction(self):
-        return f"{self.direction}"
+print(player_field.print_field())
+print(bot_field.print_field())
 
-
-class Board:
-    def __init__(self, ship_dict):
-        self.ship_dict = ship_dict
-
-    def __str__(self):
-        return f"{self.ship_dict}"
-
-    def get_dict(self):
-        return f"{self.ship_dict}"
-
-
-player_ships_list = []  # Список кораблей
-fields = []
-
-player_field = {"0": ["1", "A", "B", "C", "D", "E", "F", "1"],
-                "1": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "2": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "3": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "4": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "5": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "6": ["1", " ", " ", " ", " ", " ", " ", "1"],
-                "7": ["1", "1", "1", "1", "1", "1", "1", "1"]}
-
-bot_field = {"0": ["1", "A", "B", "C", "D", "E", "F", "1"],
-             "1": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "2": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "3": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "4": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "5": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "6": ["1", " ", " ", " ", " ", " ", " ", "1"],
-             "7": ["1", "1", "1", "1", "1", "1", "1", "1"]}
-
-
-# -------Вывод поля---------
-def print_field():
-    for i, j in player_field.items():
-        if i == "0":
-            print(" ", end="|")
-        elif i == "7":
-            pass
-        else:
-            print(i, end="|")
-        if i != "7":
-            for k in j[1:7]:
-                print(k, end="|")
-            print()
-# --------------------------
-
-def print_field_bot():
-    s_ = -1  #
-    r_ = 2  #
-    for i, j in bot_field.items():
-        if i == "0":
-            print(" ", end="|")
-        elif i == "7":
-            pass
-        else:
-            print(i, end="|")
-        if i != "7":
-            for k in j[1:7]:
-                print(k, end="|")
-            s_ += 1  #
-
-            print()
-
-
-def check_ship_around(check=1):  # Дефолт проверки одной точки
-    one_check = player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + 1] != "■" and \
-                player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] - 1] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1]] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1]] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] + 1] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] - 1] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] + 1] != "■" and \
-                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] - 1] != "■"
-    # ------------- Проверка на двойной корабль --------------
-    if check == 2:
-        if ship.get_ship_direction() == "horizontal":
-            if one_check and \
-                    player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + 2] != "■" and \
-                    player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] + 2] != "■" and \
-                    player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] + 2] != "■":
-                return True
-            else:
-                return False
-        else:
-            if one_check and \
-                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1]] != "■" and \
-                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1] + 1] != "■" and \
-                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1] - 1] != "■":
-                return True
-            else:
-                return False
-    # ---------------------------
-    else:
-        # ------------- Проверка на единичный корабль --------------
-        if one_check:
-            return True
-        else:
-            return False
-        # ---------------------------
-
-
-def ship_placement(_):
-    for h in range(_):
-        if ship.get_ship_direction() == "vertical":
-            player_field[str(ship.get_ship_nose()[0] + h)][ship.get_ship_nose()[1]] = "■"
-        else:
-            player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + h] = "■"
-
-
-def clear_field():
-    for i, j in player_field.items():
-        if i != "7":
-            for k in j[1:7]:
-                if k == "■":
-                    for h in range(1, 7):
-                        player_field[i][h] = " "
-    player_ships_list.clear()
-# -----------Генерация кораблей-----------
-def generation_ships(try_count):
-    _ = 3
-    for i in range(7):
-        try_append = True
-        if i == 1:
-            _ -= 1
-        elif i == 3:
-            _ -= 1
-        while try_append:
-            try_count += 1
-            if try_count > 50:
-                print_field()
-                return 1
-            global ship  # ------GLOBAL--------#
-            ship = Ship(_, (random.randrange(1, 7), random.randrange(1, 7)),
-                        random.choice(("vertical", "horizontal")), _)
-            print(ship.get_ship_nose(), i)
-            if player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1]] != "■" and check_ship_around():
-                if _ == 3:
-                    if ship.get_ship_nose()[0] + 2 < 7 and ship.get_ship_nose()[1] + 2 < 7:
-                        ship_placement(_)
-                    else:
-                        continue
-                elif _ == 2:
-                    if ship.get_ship_nose()[0] + 1 < 7 and ship.get_ship_nose()[1] + 1 < 7 and check_ship_around(2):
-                        ship_placement(_)
-                    else:
-                        continue
-                else:
-                    player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1]] = "■"
-                try_append = False
-            else:
-                continue
-        player_ships_list.append(ship)
-# -----------------------------------------
-
-
-for i in range(2):
-    while True:
-        if generation_ships(0) == 1:
-            clear_field()
-            continue
-        else:
-            fields.append(player_field)
-            break
-
-
-# ---Вывод инфы всех кораблей---
-for i in player_ships_list:
-    print(i.get_ship())
-# -----------------
-
-print(fields)
-print_field()  # ---Вывод поля---
-print_field_bot()  # ---Вывод поля---
-
-print()
-
-x_ = ["A", "B", "C", "D", "E", "F"]
-x_converter = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6}
-y_ = [1, 2, 3, 4, 5, 6]
-
-x = "0"
-y = 0
-
-# while True:
-#     x = input("X")
-#     y = int(input("Y"))
-#     if x in x_ and y in y_:
-#         break
+# player_field.retry_place()
+# bot_field.retry_place()
 #
-#
-# dot = Dot(x, y)
-# dot1 = Dot(x, y)
-# print(dot == dot1)
+# print(player_field.print_field())
+# print(bot_field.print_field())
+
+
+
+while True:
+    try:
+        a1 = int(input("Ваш ход: "))
+        a2 = int(input("Ваш ход: "))
+        bot_field.shot(a1, a2)
+        print(player_field.print_field())
+        print(bot_field.print_field())
+        print(bot_field.all_hp, "bot")
+        print(player_field.all_hp, "play")
+    except InSameDot:
+        print("В данную клетку уже стреляли")
---
+++ ConsoleSeaBattleOLD.py
@@ -0,0 +1,234 @@
+"""
+--Не закончена--
+--Данный файл не актуален--
+Все что есть - это генерация одного поля.
+"""
+
+import random
+import os
+
+
+class Dot:
+    def __init__(self, x: str, y: int):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+class Ship:
+    def __init__(self, width: int, nose_dot: tuple, direction: str, hp: int):
+        self.width: int = width
+        self.nose_dot: tuple = nose_dot
+        self.direction: str = direction
+        self.hp: int = hp
+
+    def dots(self):
+        return  # dots
+
+    def get_ship(self):
+        return f"{self.width} {self.nose_dot} {self.direction} {self.hp}"
+
+    def get_ship_nose(self):
+        return self.nose_dot
+
+    def get_ship_direction(self):
+        return f"{self.direction}"
+
+
+class Board:
+    def __init__(self, ship_dict):
+        self.ship_dict = ship_dict
+
+    def __str__(self):
+        return f"{self.ship_dict}"
+
+    def get_dict(self):
+        return f"{self.ship_dict}"
+
+
+player_ships_list = []  # Список кораблей
+fields = []
+
+player_field = {"0": ["1", "A", "B", "C", "D", "E", "F", "1"],
+                "1": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "2": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "3": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "4": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "5": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "6": ["1", " ", " ", " ", " ", " ", " ", "1"],
+                "7": ["1", "1", "1", "1", "1", "1", "1", "1"]}
+
+bot_field = {"0": ["1", "A", "B", "C", "D", "E", "F", "1"],
+             "1": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "2": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "3": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "4": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "5": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "6": ["1", " ", " ", " ", " ", " ", " ", "1"],
+             "7": ["1", "1", "1", "1", "1", "1", "1", "1"]}
+
+
+# -------Вывод поля---------
+def print_field():
+    for i, j in player_field.items():
+        if i == "0":
+            print(" ", end="|")
+        elif i == "7":
+            pass
+        else:
+            print(i, end="|")
+        if i != "7":
+            for k in j[1:7]:
+                print(k, end="|")
+            print()
+# --------------------------
+
+def print_field_bot():
+    s_ = -1  #
+    r_ = 2  #
+    for i, j in bot_field.items():
+        if i == "0":
+            print(" ", end="|")
+        elif i == "7":
+            pass
+        else:
+            print(i, end="|")
+        if i != "7":
+            for k in j[1:7]:
+                print(k, end="|")
+            s_ += 1  #
+
+            print()
+
+
+def check_ship_around(check=1):  # Дефолт проверки одной точки
+    one_check = player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + 1] != "■" and \
+                player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] - 1] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1]] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1]] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] + 1] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] - 1] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] + 1] != "■" and \
+                player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] - 1] != "■"
+    # ------------- Проверка на двойной корабль --------------
+    if check == 2:
+        if ship.get_ship_direction() == "horizontal":
+            if one_check and \
+                    player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + 2] != "■" and \
+                    player_field[str(ship.get_ship_nose()[0] - 1)][ship.get_ship_nose()[1] + 2] != "■" and \
+                    player_field[str(ship.get_ship_nose()[0] + 1)][ship.get_ship_nose()[1] + 2] != "■":
+                return True
+            else:
+                return False
+        else:
+            if one_check and \
+                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1]] != "■" and \
+                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1] + 1] != "■" and \
+                    player_field[str(ship.get_ship_nose()[0] + 2)][ship.get_ship_nose()[1] - 1] != "■":
+                return True
+            else:
+                return False
+    # ---------------------------
+    else:
+        # ------------- Проверка на единичный корабль --------------
+        if one_check:
+            return True
+        else:
+            return False
+        # ---------------------------
+
+
+def ship_placement(_):
+    for h in range(_):
+        if ship.get_ship_direction() == "vertical":
+            player_field[str(ship.get_ship_nose()[0] + h)][ship.get_ship_nose()[1]] = "■"
+        else:
+            player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1] + h] = "■"
+
+
+def clear_field():
+    for i, j in player_field.items():
+        if i != "7":
+            for k in j[1:7]:
+                if k == "■":
+                    for h in range(1, 7):
+                        player_field[i][h] = " "
+    player_ships_list.clear()
+# -----------Генерация кораблей-----------
+def generation_ships(try_count):
+    _ = 3
+    for i in range(7):
+        try_append = True
+        if i == 1:
+            _ -= 1
+        elif i == 3:
+            _ -= 1
+        while try_append:
+            try_count += 1
+            if try_count > 50:
+                print_field()
+                return 1
+            global ship  # ------GLOBAL--------#
+            ship = Ship(_, (random.randrange(1, 7), random.randrange(1, 7)),
+                        random.choice(("vertical", "horizontal")), _)
+            print(ship.get_ship_nose(), i)
+            if player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1]] != "■" and check_ship_around():
+                if _ == 3:
+                    if ship.get_ship_nose()[0] + 2 < 7 and ship.get_ship_nose()[1] + 2 < 7:
+                        ship_placement(_)
+                    else:
+                        continue
+                elif _ == 2:
+                    if ship.get_ship_nose()[0] + 1 < 7 and ship.get_ship_nose()[1] + 1 < 7 and check_ship_around(2):
+                        ship_placement(_)
+                    else:
+                        continue
+                else:
+                    player_field[str(ship.get_ship_nose()[0])][ship.get_ship_nose()[1]] = "■"
+                try_append = False
+            else:
+                continue
+        player_ships_list.append(ship)
+# -----------------------------------------
+
+
+for i in range(2):
+    while True:
+        if generation_ships(0) == 1:
+            clear_field()
+            continue
+        else:
+            fields.append(player_field)
+            break
+
+
+# ---Вывод инфы всех кораблей---
+for i in player_ships_list:
+    print(i.get_ship())
+# -----------------
+
+print(fields)
+print_field()  # ---Вывод поля---
+print_field_bot()  # ---Вывод поля---
+
+print()
+
+x_ = ["A", "B", "C", "D", "E", "F"]
+x_converter = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6}
+y_ = [1, 2, 3, 4, 5, 6]
+
+x = "0"
+y = 0
+
+# while True:
+#     x = input("X")
+#     y = int(input("Y"))
+#     if x in x_ and y in y_:
+#         break
+#
+#
+# dot = Dot(x, y)
+# dot1 = Dot(x, y)
+# print(dot == dot1)
