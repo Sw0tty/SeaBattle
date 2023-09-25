6d5dfe84b4f1575fef658bc549861c64c608dbe4
Swotty <sw0tty@yandex.ru>
2023-09-25 22:41:47 +0300

    Refactoring

--- console_version/main.py
+++ console_version/main.py
@@ -8,21 +8,21 @@ from app_config.initialization import board1, board2, player1, player2, board_pr
 from app_config.settings import WARNING
 
 
-def change_player(turn_player=None, new_game=False):
+def player_switcher(turn_player=None, new_game=False):
     if new_game:
         return player1 if random.randrange(0, 2) == 1 else player2
     return player2 if turn_player == player1 else player1
 
 
-now_turn_player = change_player(new_game=True)
+now_turn_player = player_switcher(new_game=True)
 
 while True:
     if now_turn_player == player2:
         board_printer.printer(now_turn_player.get_already_shoot())
     else:
-        board_printer.printer(change_player(now_turn_player).get_already_shoot())
+        board_printer.printer(player_switcher(now_turn_player).get_already_shoot())
     print('\nNow turn: ' + now_turn_player.get_name())
-    dot = now_turn_player.shot(change_player(now_turn_player).get_already_shoot())
+    dot = now_turn_player.shot(player_switcher(now_turn_player).get_already_shoot())
 
     if isinstance(dot, tuple):
         if now_turn_player == player1:
@@ -30,16 +30,16 @@ while True:
         else:
             another_shot = board1.blast_on_board(*dot)
 
-        now_turn_player = change_player(now_turn_player)
+        now_turn_player = player_switcher(now_turn_player)
 
         if not now_turn_player.alive_ships():
-            now_turn_player = change_player(now_turn_player)
+            now_turn_player = player_switcher(now_turn_player)
             break
 
-        now_turn_player = change_player(now_turn_player)
+        now_turn_player = player_switcher(now_turn_player)
 
         if not another_shot[0]:
-            now_turn_player = change_player(now_turn_player)
+            now_turn_player = player_switcher(now_turn_player)
             continue
 
         print(another_shot[1])
@@ -47,5 +47,5 @@ while True:
         print(f'[{WARNING}] ' + dot, '\n')
         time.sleep(2)
 
-board_printer.printer(change_player(now_turn_player).get_already_shoot())
+board_printer.printer(player_switcher(now_turn_player).get_already_shoot())
 print('\nVictory for: ' + now_turn_player.get_name())
